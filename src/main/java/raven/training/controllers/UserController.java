4617125db35c10ff54d0bf8c20f3fad33ae867d8
miguelcc705 <miguel.castanoc@udea.edu.co>
2025-07-23 15:50:41 -0500

    Creation of methods for add and remove books from users in user controller

--- src/main/java/raven/training/controllers/UserController.java
+++ src/main/java/raven/training/controllers/UserController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.web.bind.annotation.*;
+import raven.training.exceptions.BookNotFoundException;
 import raven.training.exceptions.UserNotFoundException;
 import raven.training.exceptions.UserIdMismatchException;
+import raven.training.models.Book;
 import raven.training.models.User;
+import raven.training.repositories.BookRepository;
 import raven.training.repositories.UserRepository;
 
 import java.util.List;
@@ -17,6 +20,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
 
     @GetMapping("/{id}")
     public User findOne(@PathVariable Long id) {
@@ -49,6 +55,26 @@ public class UserController {
         }
         userRepository.findById(id)
                 .orElseThrow(UserNotFoundException::new);
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{userId}/books/{bookId}")
+    public User addBookToUser(@PathVariable Long userId, @PathVariable Long bookId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("Usuario no encontrado"));
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new BookNotFoundException("Libro no encontrado"));
+        user.addBook(book);
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/{userId}/books/{bookId}")
+    public User removeBookFromUser(@PathVariable Long userId, @PathVariable Long bookId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("Usuario no encontrado"));
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new BookNotFoundException("Libro no encontrado"));
+        user.removeBook(book);
         return userRepository.save(user);
     }
 
